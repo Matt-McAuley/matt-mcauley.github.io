130b361c1032c7ce1dea8ba7d3b12e9fe89cdea3
Matt-McAuley <mattwilliam618@gmail.com>
2025-01-11 23:55:56 +0000

    Deploying to gh-pages from @ Matt-McAuley/matt-mcauley.github.io@bf06bbbc0d24a9c1746ad402b2d917f5cb7abdc7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-resume",
-          title: "Resume",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/resume/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "Projects",
           description: "",
           section: "Navigation",
